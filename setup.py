8e0969d748a600cc9d71d8a6b50234d36303818e
anthonyastarita <38874472+anthonyastarita@users.noreply.github.com>
2020-11-09 17:34:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'DividendFiend',         # How you named your package folder (MyLib)
   packages = ['DividendFiend'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Dividend Stock Data',   # Give a short description about your library
   author = 'Anthony Astarita',                   # Type in your name
