a02e44d4e853cd70baebe6a1b2be5035e2fa40e9
anthonyastarita <38874472+anthonyastarita@users.noreply.github.com>
2020-11-10 13:20:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Anthony Astarita',                   # Type in your name
   author_email = 'anthonyastarita98@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/anthonyastarita/DividendFiend/archive/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/anthonyastarita/DividendFiend/archive/0.3.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'yfinance',
