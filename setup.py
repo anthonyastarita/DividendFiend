505dc16eb7527081054ce94c04c33c04bdd73092
anthonyastarita <38874472+anthonyastarita@users.noreply.github.com>
2020-11-10 22:35:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
   name = 'DividendFiend',         # How you named your package folder (MyLib)
   packages = ['DividendFiend'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Dividend Stock Data',   # Give a short description about your library
   author = 'Anthony Astarita',                   # Type in your name
   author_email = 'anthonyastarita98@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/anthonyastarita/DividendFiend/archive/0.4.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  download_url = 'https://github.com/anthonyastarita/DividendFiend/archive/0.5.tar.gz',    # I explain this later on
+  keywords = ['dividend', 'dividends', 'scraper', 'dividend growth investing', 'DGI', 'passive income', 'stocks', 'stock market'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'yfinance',
           'pandas',
